5b0f389ddfcc6a0b292ed2eb6531298ed4bb48a7
David <dbiderman@teridion.com>
2021-10-30 17:12:27 +0300

    remove dist folder

--- dist/services/mock-service.d.ts
+++
@@ -1,5 +0,0 @@
-export default class MockService {
-    private static _data;
-    static getData: () => Array<string>;
-}
-//# sourceMappingURL=mock-service.d.ts.map
--- dist/utils/keymapper.utils.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    ESCAPE_KEY: number;
-    UP_ARROW: number;
-    DOWN_ARROW: number;
-    ENTER_KEY: number;
-};
-export default _default;
-//# sourceMappingURL=keymapper.utils.d.ts.map
